aabecb7c5f392b0c94495f6639ca950970bba8f6
Joshua Meadows <jmeadows4@mail.csuchico.edu>
2021-05-10 00:23:02 -0700

    update plot to show radius changing

--- yearly_temps_animation.py
+++ yearly_temps_animation.py
@@ -60,29 +60,51 @@ plt.xlim(26, 34)
 plt.show()
 
 
+
+########## Homology Stuff ###########
+
+
+point_cloud = np.vstack((yearly_mintemps,yearly_maxtemps)).T
+
+# Call fill_rips to compute the rips filtrations. 5 is the number of dimensions,
+# 8 is the radius of the balls. It's an arbitrary value
+f = d.fill_rips(point_cloud, 5, 0.5)
+
+# Plot the diagram and barcodes from the vietoris-rips filtrations above. This
+# code is pretty much straight ripped from dionysus documentation
+p = d.homology_persistence(f)
+dgms = d.init_diagrams(p,f)
+d.plot.plot_diagram(dgms[1], show=True)
+d.plot.plot_bars(dgms[1],show=True)
+
+
+######################################
+
 ### All the code below is for animation ###
 
-fig = plt.figure()
-ax = plt.axes()
+fig = plt.figure(figsize=(8,8))
+ax = plt.subplot(aspect='equal')
 # Plot the static points, no need to animate anything
 plt.scatter(yearly_mintemps,yearly_maxtemps, s=10)
 ax.set_xlim(26,34)
 ax.set_ylim(51,61)
-# Create a scatter object, which we will update. The orange circles are actually
-# just scattered points with low alpha value and increasing size
-scat = ax.scatter(yearly_mintemps, yearly_maxtemps, s=10, alpha=0.3)
+txt = plt.text(26,60,'Radius:', fontsize=10)
 
-# Create an array of radius sizes. This is the format that the set_sizes function
-# below expects
-radius_sizes = len(yearly_mintemps) * [10]
+# Create a circle array to make the orange circles
+circles = []
+for min_t, max_t in zip(yearly_mintemps,yearly_maxtemps):
+    circle = plt.Circle((min_t,max_t),0.3,alpha=0.2,color='orange')
+    ax.add_patch(circle)
+    circles.append(circle)
+
 
 # The main function that is called on each frame of the animation. The only goal
-# of the function is to update the radius of the balls in the scatter plot
+# of the function is to update the radius of the circles in the scatter plot
 def update(frame_number):
-    for i in range(len(radius_sizes)):
-        radius_sizes[i] = 5*frame_number+10
-    scat.set_sizes(radius_sizes)
+    for circle in circles:
+        circle.set_radius(frame_number*0.005)
+    txt.set_text('Radius: {}'.format(frame_number*0.005))
 
 # start animation
-animation = FuncAnimation(fig, update, frames = 500,interval=10)
+animation = FuncAnimation(fig, update, frames = 200,interval=20)
 plt.show()
